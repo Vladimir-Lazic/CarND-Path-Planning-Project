af6f525f617ac672194bff2fd718b6029488c8a8
Vladimir Lazic <vladimirzlazic@gmail.com>
2021-01-02 18:22:40 +0100

    Basic smoth path implementation

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -58,6 +59,11 @@ int main()
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
+
+        int lane = 1;
+
+        double ref_val = 49.5;
+
         if (length && length > 2 && data[0] == '4' && data[1] == '2')
         {
 
@@ -94,13 +100,105 @@ int main()
 
                     json msgJson;
 
+                    int prev_size = previous_path_x.size();
+
+                    // Add sensor fusion data here
+
+                    // Create a list of widely spaced waypoints
+                    vector<double> ptsx;
+                    vector<double> ptsy;
+
+                    double ref_x = car_x;
+                    double ref_y = car_y;
+                    double ref_yaw = deg2rad(car_yaw);
+
+                    if (prev_size < 2)
+                    {
+                        double prev_car_x = car_x - cos(car_yaw);
+                        double prev_car_y = car_y - sin(car_yaw);
+
+                        ptsx.push_back(prev_car_x);
+                        ptsx.push_back(car_x);
+
+                        ptsy.push_back(prev_car_y);
+                        ptsy.push_back(car_y);
+                    }
+                    else
+                    {
+                        ref_x = previous_path_x[prev_size - 1];
+                        ref_y = previous_path_y[prev_size - 1];
+
+                        double prev_ref_x = previous_path_x[prev_size - 2];
+                        double prev_ref_y = previous_path_y[prev_size - 2];
+                        ref_yaw = atan2(ref_y - prev_ref_y, ref_x - prev_ref_x);
+
+                        ptsx.push_back(prev_ref_x);
+                        ptsx.push_back(ref_x);
+
+                        ptsy.push_back(prev_ref_y);
+                        ptsy.push_back(ref_y);
+                    }
+
+                    vector<double> next_wp0 = getXY(car_s + 30, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                    vector<double> next_wp1 = getXY(car_s + 60, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                    vector<double> next_wp2 = getXY(car_s + 90, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+                    ptsx.push_back(next_wp0[0]);
+                    ptsx.push_back(next_wp1[0]);
+                    ptsx.push_back(next_wp2[0]);
+
+                    ptsy.push_back(next_wp0[1]);
+                    ptsy.push_back(next_wp1[1]);
+                    ptsy.push_back(next_wp2[1]);
+
+                    for (int i = 0; i < ptsx.size(); i++)
+                    {
+                        double shift_x = ptsx[i] - ref_x;
+                        double shift_y = ptsy[i] - ref_y;
+
+                        ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+                        ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+                    }
+
+                    tk::spline spline;
+
+                    spline.set_points(ptsx, ptsy);
+
                     vector<double> next_x_vals;
                     vector<double> next_y_vals;
 
-                    /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+                    for (int i = 0; i < prev_size; i++)
+                    {
+                        next_x_vals.push_back(previous_path_x[i]);
+                        next_y_vals.push_back(previous_path_y[i]);
+                    }
+
+                    double target_x = 30.0;
+                    double target_y = spline(target_x);
+                    double target_dist = sqrt(target_x * target_x + target_y * target_y);
+
+                    double x_add_on = 0;
+
+                    for (int i = 1; i <= 50 - prev_size; i++)
+                    {
+                        double N = target_dist / (0.02 * ref_val / 2.24);
+                        double x_point = x_add_on + target_x / N;
+                        double y_point = spline(x_point);
+
+                        x_add_on = x_point;
+
+                        double x_ref = x_point;
+                        double y_ref = y_point;
+
+                        x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+                        y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+                        x_point += ref_x;
+                        y_point += ref_y;
+
+                        next_x_vals.push_back(x_point);
+                        next_y_vals.push_back(y_point);
+                    }
 
                     msgJson["next_x"] = next_x_vals;
                     msgJson["next_y"] = next_y_vals;
